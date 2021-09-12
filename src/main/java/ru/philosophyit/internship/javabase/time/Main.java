27b3434444abfe5c4e75564f0f45e5839e242ab1
ASURA-KaMi <fairee813@gmail.com>
2021-09-13 01:38:43 +0300

    Added solve the calendar task Output: MON  TUE  WED  THU  FRI  SAT  SUN  30   31    1    2    3    4    5   6    7    8    9   10   11   12  13*  14   15   16   17   18   19  20   21   22   23   24   25   26  27   28   29   30

--- src/main/java/ru/philosophyit/internship/javabase/time/Main.java
+++ src/main/java/ru/philosophyit/internship/javabase/time/Main.java
@@ -1,16 +1,62 @@
 package ru.philosophyit.internship.javabase.time;
 
 import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
+import java.util.*;
+
+class ConsoleCalendar{
+    private static List<String> calendar_matrix = new ArrayList<>();
+    private void createCalendar(){
+        Calendar cal = new GregorianCalendar();
+        int today = cal.get(Calendar.DAY_OF_MONTH);
+        cal.add(Calendar.DAY_OF_MONTH, - (today - 1)); //Back to first day of month
+
+        String num = ""; //Our day field
+        int day_offset = cal.get(Calendar.DAY_OF_WEEK) - 3;
+        int day_of_week = 0;
+        int days_in_month = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+
+        cal.add(Calendar.MONTH, -1); //To get amount days in past month
+        int days_in_past_month = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+
+        for (int i = -day_offset; i <= days_in_month; i++){  //Building calendar view structure
+            if (i <= 0){
+                num = " " + (days_in_past_month + i);
+            }else if (( i / 10) == 0 ){
+                num = "  " + i;
+            }else
+                num = " " + i;
+
+
+            if (i == today)
+                num +="* "; //Marking this day
+            else
+                num +="  ";
+
+
+            calendar_matrix.add(num);
+            num = "";
+
+
+            day_of_week++;
+
+            if (day_of_week == 7){
+                calendar_matrix.add("\n");
+                day_of_week = 0;
+            }
+        }
+    }
+    public void drawCalendar(){
+        createCalendar();
+        System.out.println("MON  TUE  WED  THU  FRI  SAT  SUN");
+        for(String string: calendar_matrix)
+            System.out.print(string);
+    }
+
+}
 
 public class Main {
     public static void main(String ...args) {
-        System.out.println(DateTimeFormatter.ISO_INSTANT.format(Calendar.getInstance().toInstant()));
-
-        // Отобразите календарь текущего месяца в консоли
-        // например:
-        // пн вт ср чт пт сб вс
-        // 30 31 1  2  3  4  5
-        // и т.д.
+        ConsoleCalendar conCal = new ConsoleCalendar();
+        conCal.drawCalendar();
     }
 }
