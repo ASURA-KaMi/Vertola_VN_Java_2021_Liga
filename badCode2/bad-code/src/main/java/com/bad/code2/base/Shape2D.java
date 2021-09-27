11c1897b50be79a81a55ea5b3e6bd17a82e43a2f
asura <fairee813@gmail.com>
2021-09-27 23:11:38 +0300

    Ehh

--- badCode2/bad-code/src/main/java/com/bad/code2/base/Shape2D.java
+++
@@ -1,7 +0,0 @@
-package com.bad.code2.base;
-
-public interface Shape2D {
-    Point getPos();
-    Point getCenter();
-    Double getPerimeter();
-}
--- badCode2/bad-code/src/main/java/com/bad/code2/base/Shape3D.java
+++
@@ -1,7 +0,0 @@
-package com.bad.code2.base;
-
-public interface Shape3D {
-    Point3D getPos();
-    Point3D getCenter();
-    Double getVolume();
-}
